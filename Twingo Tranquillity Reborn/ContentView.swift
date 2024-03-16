cf4f8527dbee6d1a565310a2c49f9ee4242967bf
Goosander Mihail <claudiu.grosan@gmail.com>
2024-03-16 16:05:40 +0200

    Initial Commit

---
+++ Twingo Tranquillity Reborn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Twingo Tranquillity Reborn
+//
+//  Created by Clau on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Twingo Tranquillity Reborn/Twingo_Tranquillity_RebornApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twingo_Tranquillity_RebornApp.swift
+//  Twingo Tranquillity Reborn
+//
+//  Created by Clau on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Twingo_Tranquillity_RebornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

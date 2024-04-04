2de8e70ca9f5e8dcd3bb5ebe2ff1a1ee6b865748
Goosander Mihail <claudiu.grosan@gmail.com>
2024-04-04 23:41:43 +0300

    more work mostly on mk2FaceliftAbt

--- Twingo Tranquillity Reborn/mk2/mk2f/Mk2FaceliftAbout.swift
+++ Twingo Tranquillity Reborn/mk2/mk2f/Mk2FaceliftAbout.swift
@@ -23,6 +23,7 @@ struct Mk2FaceliftAbout: View {
                 .fontDesign(.rounded)
                 .frame(maxWidth: 350, alignment: .leading)
             ScrollView(.vertical, showsIndicators: false, content: {
+                
                 VStack{
                     Image("mk2FaceliftImg")
                         .resizable(capInsets: EdgeInsets(top:10, leading: 40, bottom: 40, trailing: 50))
@@ -43,7 +44,7 @@ struct Mk2FaceliftAbout: View {
                                     .padding(5)
                                 
                                 Text("""
-                După prezentarea unui concept inițial la Salonul "Mondial de l'Automobile" din 2006, Renault a debutat modelul de serie la Salonul Auto de la Geneva din 2007, cu niveluri de echipare pentru piața franceză numite Authentique, Expression, Initiale, Dynamique și GT.
+                În iulie 2011, Renault a prezentat un Twingo II facelift la Salonul Auto de la Frankfurt, cu un limbaj de design utilizat ulterior pe întreaga gamă, care consta într-o nouă iterație a imaginii exterioare, precum și blocuri optice față și spate redefinite.
                 """).frame(maxWidth: 370, maxHeight: 190)
                                     .lineSpacing(5)
                                     .multilineTextAlignment(.leading)
@@ -57,28 +58,12 @@ struct Mk2FaceliftAbout: View {
                                     .padding(5)
                                 
                                 Text("""
-                    Folosind platforma Clio II, Twingo II a oferit o protecție îmbunătățită la impact și a fost disponibil atât în configurații cu volan pe dreapta, cât și pe stânga. Producția a început în Franța și a fost mutată ulterior la fabrica Revoz din Novo Mesto, Slovenia.
+                    Facelift-ul a rămas o mașină de oraș practică și ușor de manevrat, fiind concepută drept o opțiune accesibilă. Configurațiile inteligente ale scaunelor și opțiunea de a transforma scaunul pasagerului într-o masă de picnic ofereau o funcționalitate unică interiorului conceput pentru practicalitate.
                     """)
                                 .frame(maxWidth: 370, maxHeight: 190)
                                 .lineSpacing(5)
                                 .multilineTextAlignment(.leading)
                                 .fontDesign(.rounded)
-                            }
-                            ZStack{
-                                RoundedRectangle(cornerRadius:23)
-                                    .stroke(Color.customPink, lineWidth: 5)
-                                    .frame(width: 380, height: 180)
-                                    .shadow(radius: 10)
-                                    .padding(5)
-                                
-                                Text("""
-                    În ianuarie 2008, Renault a prezentat la Salonul Auto de la Geneva, Twingo Renaultsport (RS) 133, cu un motor nou de 1.598 cmc ce dezvoltă 133 CP (98 kW). Comenzile și producția modelului RS au încetat în august 2013.
-                    """)
-                                .frame(maxWidth: 370, maxHeight: 190)
-                                .lineSpacing(5)
-                                .multilineTextAlignment(.leading)
-                                .fontDesign(.rounded)
-                                
                             }
                             
                         }
@@ -98,7 +83,7 @@ struct Mk2FaceliftAbout: View {
                                     .padding(5)
                                 
                                 Text("""
-                Recenzile vremii au apreciat ca puncte forte ale interiorului noului Twingo faptul că designul bordului este unul atractiv și faptul că include acum un turometru, dar și vitezometrul digital, amplasat central.
+                La interior nu s-au schimbat prea multe, dar Twingo s-a îmbunătățit față de aspectul său mai degrabă utilitar. Renault a înțeles faptul că multimedia și conectivitatea sunt esențiale pentru cumpărători.
                 """)
                                 .frame(maxWidth: 370, maxHeight: 190)
                                 .lineSpacing(5)
@@ -114,27 +99,11 @@ struct Mk2FaceliftAbout: View {
                                     .padding(5)
                                 
                                 Text("""
-                Totuși, jurnaliștii vremii au descris interiorul ca unul modest, chiar "spartan" datorită abundenței plasticului în componența sa, totuși ceva normal și de așteptat pentru un autovehicul accesibil din punct de vedere al prețului.
+                Astfel, Twingo primea standard Bluetooth, intrări audio și un CD player, conexiune USB, patru difuzoare, jante din aliaj, cruise control, aer condiționat și limitator de viteză.
                 """).frame(maxWidth: 370, maxHeight: 190)
                                     .lineSpacing(5)
                                     .multilineTextAlignment(.leading)
                                     .fontDesign(.rounded)
-                            }
-                            ZStack{
-                                RoundedRectangle(cornerRadius:23)
-                                    .stroke(Color.customPink, lineWidth: 5)
-                                    .frame(width: 380, height: 180)
-                                    .shadow(radius: 10)
-                                    .padding(5)
-                                
-                                Text("""
-                Ca opțiuni, se remarcă volanul și schimbătorul din piele, iar nivelul de echipare standard includea: aer condiționat, CD player cu conectivitate pentru iPod și scaune spate rabatabile.
-                """)
-                                .frame(maxWidth: 360, maxHeight: 190)
-                                .lineSpacing(5)
-                                .multilineTextAlignment(.leading)
-                                .fontDesign(.rounded)
-                                
                             }
                         }
                     })

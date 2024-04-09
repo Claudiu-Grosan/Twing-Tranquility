f0dc0e1606089e953885157d0028a475579017f6
Goosander Mihail <claudiu.grosan@gmail.com>
2024-04-09 17:57:19 +0300

    more multi device support

---
+++ Twingo Tranquillity Reborn/mk2/Mk2SelectionSpecs.swift
@@ -0,0 +1,18 @@
+//
+//  Mk2SelectionSpecs.swift
+//  Twingo Tranquillity Reborn
+//
+//  Created by Clau on 4/9/24.
+//
+
+import SwiftUI
+
+struct Mk2SelectionSpecs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mk2SelectionSpecs()
+}
---
+++ Twingo Tranquillity Reborn/mk2/Mk2Specs_iPad.swift
@@ -0,0 +1,18 @@
+//
+//  Mk2Specs_iPad.swift
+//  Twingo Tranquillity Reborn
+//
+//  Created by Clau on 4/9/24.
+//
+
+import SwiftUI
+
+struct Mk2Specs_iPad: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mk2Specs_iPad()
+}

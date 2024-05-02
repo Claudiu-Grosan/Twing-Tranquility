9f0adacf96f7cecf2c986135ebf2e6d41a473ac7
Goosander Mihail <claudiu.grosan@gmail.com>
2024-05-02 22:51:35 +0300

    finished mk3About (hopefully)

---
+++ Twingo Tranquillity Reborn/mk3/Mk3About_iPad.swift
@@ -0,0 +1,18 @@
+//
+//  Mk3About_iPad.swift
+//  Twingo Tranquillity Reborn
+//
+//  Created by Clau on 5/2/24.
+//
+
+import SwiftUI
+
+struct Mk3About_iPad: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mk3About_iPad()
+}
---
+++ Twingo Tranquillity Reborn/mk3/Mk3Selection.swift
@@ -0,0 +1,18 @@
+//
+//  Mk3Selection.swift
+//  Twingo Tranquillity Reborn
+//
+//  Created by Clau on 5/2/24.
+//
+
+import SwiftUI
+
+struct Mk3Selection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Mk3Selection()
+}
